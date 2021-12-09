9341331f35d8ad218f9083a221a61dfe7c436bca
Bas Paap <baspaap@live.nl>
2021-12-09 21:41:44 +0100

    Add receive code.

--- CasinoMortale.KeypadDecoder/CasinoMortale.KeypadDecoder.ino
+++ CasinoMortale.KeypadDecoder/CasinoMortale.KeypadDecoder.ino
@@ -5,11 +5,53 @@
 */
 
 // the setup function runs once when you press reset or power the board
-void setup() {
+#include <SoftwareSerial.h>
 
+const int rxPin = 9;
+const int txPin = 10;
+SoftwareSerial softwareSerial(rxPin, txPin);
+
+char receivedChars[7];
+int numCharsWritten;
+bool isWritingCharacters;
+bool isPinCodeReceived;
+
+void setup() {
+	Serial.begin(9600);
+		
+	softwareSerial.begin(4800);
+	softwareSerial.flush();
 }
 
 // the loop function runs over and over again until power down or reset
 void loop() {
-  
+
+	if (!isPinCodeReceived && softwareSerial.available())
+	{		
+		char character = softwareSerial.read();
+
+		if (character == '*')
+		{
+			isWritingCharacters = true;
+		}
+		else if (isWritingCharacters)
+		{
+			if (character == '#' || numCharsWritten == 6)
+			{
+				receivedChars[numCharsWritten] = '\0';
+				isWritingCharacters = false;
+				isPinCodeReceived = true;
+			}
+			else
+			{
+				receivedChars[numCharsWritten] = character;
+				numCharsWritten++;
+			}			
+		}
+	}
+	else
+	{
+		Serial.print("Received pinCode:");
+		Serial.println(receivedChars);
+	}
 }
